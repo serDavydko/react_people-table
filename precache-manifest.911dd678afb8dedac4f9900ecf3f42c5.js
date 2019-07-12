80bb536356a8208170ac9f68e2270d9bb6cb269e
Serhii <davydko.serhiy@gmail.com>
2019-07-12 15:36:58 +0300

    Updates

---
+++ precache-manifest.911dd678afb8dedac4f9900ecf3f42c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e62bea7b3569bb13ce2c7c9bf7f542",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "ba00adc47e601c94c79a",
+    "url": "/react_people-table/static/css/main.ad63845c.chunk.css"
+  },
+  {
+    "revision": "bc7ee59200c250f6fc22",
+    "url": "/react_people-table/static/js/2.08ab55ae.chunk.js"
+  },
+  {
+    "revision": "ba00adc47e601c94c79a",
+    "url": "/react_people-table/static/js/main.fb8208ab.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
